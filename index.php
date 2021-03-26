9e5bd81ea1dc344f2843676188c7481846ea61a3
Natalie Stout <natalie_stout99@hotmail.com>
2021-03-26 00:34:40 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require 'connection.php';
+
+//additional php code for this page goes here
+
+?>
+
+    <!-- START PAGE CONTENT -->
+    <h1 class="title">Home page</h1>
+    <p>This is where page content goes.</p>
+    <!-- END PAGE CONTENT -->
---
+++ setenv.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+$_SESSION['SQLUSER'] = "csteam4un@cs3620-team4finalproject";
+$_SESSION['SQLPW'] = "Csteam4pw";
+
+?>
