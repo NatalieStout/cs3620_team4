8a5b60dd92c7067ebf5620513bb64b54665d27f6
Daniel Baird <oldschoolskill19@gmail.com>
2021-03-28 20:50:15 -0600

    setting up users, tasks, and session code

---
+++ user/userDAO.php
@@ -0,0 +1,77 @@
+<?php
+class UserDAO {
+  function getUser($user){
+    require_once('./utilities/connection.php');
+    
+    $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE user_id =" . $user->getUserId();
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $user->setFirstName($row["first_name"]);
+        $user->setLastName($row["last_name"]);
+        $user->setUsername($row["username"]);
+    }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+  }
+
+  function checkLogin($passedinusername, $passedinpassword){
+    require_once('./utilities/connection.php');
+    $user_id = 0;
+    $sql = "SELECT user_id FROM users WHERE username = '" . $passedinusername . "' AND password = '" . hash("sha256", trim($passedinpassword)) . "'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        $user_id = $row["user_id"];
+      }
+    }
+    else {
+        echo "0 results";
+    }
+    $conn->close();
+    return $user_id;
+  }
+
+  function createUser($user){
+    require_once('./utilities/connection.php');
+
+    // prepare and bind
+    $stmt = $conn->prepare("INSERT INTO team4project.user (`username`,
+    `password`,
+    `first_name`,
+    `last_name`) VALUES (?, ?, ?, ?)");
+
+    $un = $user->getUsername();
+    $pw = $user->getPassword();
+    $fn = $user->getFirstName();
+    $ln = $user->getLastName();
+
+    $stmt->bind_param("ssss", $un, $pw, $fn, $ln);
+    $stmt->execute();
+   
+    $stmt->close();
+    $conn->close();
+    echo "User Created";
+  }
+
+  function deleteUser($un){
+    require_once('./utilities/connection.php');
+    
+    $sql = "DELETE FROM CS3620.users WHERE username = '" . $un . "';";
+    
+    echo $sql;
+    $result = $conn->query($sql);
+
+    $conn->close();
+    echo "User Deleted";
+  }
+
+  
+}
+?>
