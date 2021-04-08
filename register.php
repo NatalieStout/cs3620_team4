f4126002f3b49a7ab3bbd9400dd482435f5a76a2
Daniel Baird <oldschoolskill19@gmail.com>
2021-04-08 17:31:32 -0600

    fixed register to require

--- register.php
+++ register.php
@@ -8,10 +8,10 @@
      <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
 <form method="POST" action="./user_insert.php" >
-    Username:<input class="input is-normal" type="text" name="username" placeholder="Username" /><br />
-    First Name:<input class="input is-normal" type="text" name="firstName" placeholder="First Name" /><br />
-    Last Name:<input class="input is-normal" type="text" name="lastName" placeholder="Last Name" /><br />
-    Password:<input class="input is-normal" type="password" name="password" placeholder="*****" /><br /><br />
+    Username:<input class="input is-normal" type="text" name="username" placeholder="Username" required/><br />
+    First Name:<input class="input is-normal" type="text" name="firstName" placeholder="First Name" required/><br />
+    Last Name:<input class="input is-normal" type="text" name="lastName" placeholder="Last Name" required/><br />
+    Password:<input class="input is-normal" type="password" name="password" placeholder="*****" required/><br /><br />
     <input class="button is-info" type="submit" value="Create User" />
 </form>
 
