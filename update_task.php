8f28e68c1b8bc9215a08584de9be7676c81ad398
sydney13 <59878754+sydney13@users.noreply.github.com>
2021-04-16 12:34:12 -0600

    update task

--- update_task.php
+++ update_task.php
@@ -1,7 +1,29 @@
 <?php
 require 'utilities/connection.php';
 require_once('./header.php');
+if(isset($_GET['id'])){
+    //get the contact from the database
+    $stmt = $pdo->prepare('SELECT * FROM tasks WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    if(!$contact){
+        exit('Task does not exist');
+    }
 
+    if(!empty($_POST)) {
+        $task_name = isset($_POST['task_name']) ? $_POST['task_name'] : '';
+        $task_start = isset($_POST['task_start']) ? $_POST['task_start'] : '';
+        $task_end = isset($_POST['task_end']) ? $_POST['task_end'] : '';
+        $task_description = isset($_POST['task_description']) ? $_POST['task_description'] : '';
+
+        $stmt = $pdo->prepare('UPDATE tasks SET id = ?, task_name = ?, task_start = ?, task_end = ?, task_description = ? WHERE id = ?');
+        $stmt->execute([$_GET['id'], $task_name, $task_start, $task_end, $task_description, $_GET['id']]);
+        $msg = 'Update successfully!';
+    }
+
+} else {
+    exit('No ID specified');
+}
 
 
 ?>
@@ -10,8 +32,8 @@ require_once('./header.php');
 
     <!-- START PAGE CONTENT -->
     <h1 class="title">Task Update</h1>
+    <form method="POST" action="insert_task.php"> 
 
-<form method="POST" action="insert_task.php"> 
     <div class="input-group flex-nowrap">
         <span class="input-group-text" id="addon-wrapping">Task Name</span>
         <input type="text" class="form-control" name="task_name" placeholder="Task Name"/>
