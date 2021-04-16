213d92c872fc40c96a22640ed6f4a7ac9d2c4a60
sydney13 <59878754+sydney13@users.noreply.github.com>
2021-04-16 14:00:45 -0600

    Added some styles and update to task card

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
+
 <?php
 require_once('./header.php');
-
+echo '<h1 class="title" style="margin-top: 10px; margin-left: 50px;">Tasks</h1>';
 
 if(isset($_GET["del"]) AND $_GET["del"] == "true"){
     echo "<script>alert('task was deleted!');</script>";
@@ -17,13 +18,15 @@ if(isset($_GET["del"]) AND $_GET["del"] == "true"){
           $arrlength = count($tasks);
   
           for($x = 0; $x < $arrlength; $x++) {
-              echo '<div class="card" style="width: 18rem;">
+              echo '<div class="card" style="width: 18rem; padding: 30px; margin-top: 20px; margin-left: 50px;">
                       <div class="card-body">
-                          <h5 class="card-title">' . $tasks[$x]->getTaskName() . '</h5>
+                          <h5 class="card-title" style="font-size: 20px; font-weight: bold;">' . $tasks[$x]->getTaskName() . '</h5>
                           <h6 class="card-subtitle mb-2 text-muted">Start: ' . $tasks[$x]->getTaskStart() . '</h6>
                           <h6 class="card-subtitle mb-2 text-muted">End: ' . $tasks[$x]->getTaskEnd() . '</h6>
                           <p class="card-text">' . $tasks[$x]->getTaskDesc() . '</p>
-                          <a href="delete_task.php?task_id=' . $tasks[$x]->getTaskId() . '" class="card-link">Delete Task</a>
+                          <br>
+                          <a style="color: red;" href="delete_task.php?task_id=' . $tasks[$x]->getTaskId() . '" class="card-link">Delete</a>
+                          <a style="padding: 10px; color: #23d160;" href="update_task.php?task_id=' . $tasks[$x]->getTaskId() . '" class="card-link">Update</a>
 
                       </div>
                     </div>
@@ -31,7 +34,6 @@ if(isset($_GET["del"]) AND $_GET["del"] == "true"){
           }
 
 ?>
-
 
 
 
