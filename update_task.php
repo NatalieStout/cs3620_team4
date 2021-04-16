925b01ceb6abecd1f97577ece8a28effff51965f
sydney13 <59878754+sydney13@users.noreply.github.com>
2021-04-16 16:14:21 -0600

    Not working....

--- update_task.php
+++ update_task.php
@@ -1,5 +1,4 @@
 <?php
-require './utlities/connection.php';
 require_once('./header.php');
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -11,35 +10,43 @@ require_once('./task/task.php');
 $task = new task();
 $tasks = $task->updateTask($_GET["task_id"], $_SESSION["user_id"]);  
 
-header("Location: ./dashboard.php?update=true");
+
 
 ?>
 
-<h1 class="title">Task Update</h1>
-    <form method="POST" action="insert_task.php"> 
-
-    <div class="input-group flex-nowrap">
-        <span class="input-group-text" task_id="addon-wrapping">Task Name</span>
-        <input type="text" class="form-control" name="task_name" placeholder="Task Name"/>
-    </div>
-    <div class="input-group">
-        <span class="input-group-text" task_id="addon-wrapping">Start Date</span>
-        <input type="datetime-local" class="form-control" name="task_start"/>
-    </div>
-    <div class="input-group">
-        <span class="input-group-text" task_id="addon-wrapping">End Date</span>
-        <input type="datetime-local" class="form-control" name="task_end"/>
-    </div>
-    <label class="input-group-text">Task Description</label>
-    <div class="input-group">
-        
-        <textarea cols=50 rows=10 class="form-control" name="task_description"></textarea>
-    </div>
-    <div class="field">
-        <div class="control">
-            <input type="submit" class="button" value="update">
-        </div>
-    </div>
-</form>
-
+<div style="margin: 150px; margin-top: 25px;">
+<h1 class="title" >Task Update</h1>
+<form action="" method="POST">
+            <div class="field">
+                <label class="label">Task Name</label>
+                <div class="control">
+                <input type="text" class="form-control" name="task_name" placeholder="Task Name"/>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Start Date</label>
+                <div class="control">
+                <input type="datetime-local" class="form-control" name="task_start"/>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">End Date</label>
+                <div class="control">
+                <input type="datetime-local" class="form-control" name="task_end"/>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Task Description</label>
+                <div class="control">
+                <textarea width="50pxpx" class="textarea" name="task_description" placeholder="Task description"></textarea>
+                </div>
+            </div>
+            <div class="field">
+                <div class="control">
+                    <input type="submit" class="button is-success" value="Update Task">
+                </div>
+            </div>
+        </form>
+</div>
 <?= require_once('./footer.php') ?>
+
