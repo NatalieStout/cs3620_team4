a472d8d9c2d2f7b014e695a14f89bf0616d0774d
sydney13 <59878754+sydney13@users.noreply.github.com>
2021-04-19 13:44:24 -0600

    made all the forms look the same

--- create_task.php
+++ create_task.php
@@ -6,40 +6,39 @@ $family = $family_member->getFamilyMembers($_SESSION['user_id']);
 
 $familylength = count($family);
 
-echo '<link href="~bulma-calendar/dist/css/bulma-calendar.min.css" rel="stylesheet">
-<div style="margin: 150px; margin-top: 25px;">
+echo '<div style="margin: 150px; margin-top: 25px;">
 <h1 class="title" >Create Task</h1>
 <form action="insert_task.php" method="POST">
-            <div class="field">
+            <div class="field" style="width: 300px;">
                 <label class="label">Task Name</label>
                 <div class="control">
-                <input type="text" class="form-control" name="task_name" placeholder="Task Name"/>
+                <input type="text" class="input" name="task_name" placeholder="Task Name"/>
                 </div>
             </div>
             <div class="field">
         <label class="label">Select Family Member</label>
-            <div class="control">
+            <div class="select">
                 <select name="family_member_id">
-                    <option value="0">Select dropdown</option>';
+                    <option value="0">Select Family Member</option>';
                     for($y = 0; $y < $familylength; $y++){
                         echo '<option value="'. $family[$y]->getFamilyMemberId() .'">' . $family[$y]->getFamilyMemberName() . '</option>';
                         }
             echo '</select>
             </div>
         </div>
-            <div class="field">
+            <div class="field" style="width: 250px;">
                 <label class="label">Start Date</label>
                 <div class="control">
-                <input type="datetime-local" class="form-control" name="task_start"/>
+                <input type="datetime-local" class="input" name="task_start"/>
                 </div>
             </div>
-            <div class="field">
+            <div class="field" style="width: 250px;">
                 <label class="label">End Date</label>
                 <div class="control">
-                <input type="datetime-local" class="form-control" name="task_end"/>
+                <input type="datetime-local" class="input" name="task_end"/>
                 </div>
             </div>
-            <div class="field">
+            <div class="field" style="width: 600px;">
                 <label class="label">Task Description</label>
                 <div class="control">
                 <textarea width="50pxpx" class="textarea" name="task_description" placeholder="Task description"></textarea>
@@ -51,8 +50,9 @@ echo '<link href="~bulma-calendar/dist/css/bulma-calendar.min.css" rel="styleshe
                 </div>
             </div>
         </form>
+        <br>
+        <br>
 </div';
-?>
-<?php
+
 require_once('./footer.php')
 ?>
