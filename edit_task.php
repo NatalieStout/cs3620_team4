61b6b7a5321947d0634d1f9e6aea2fc75454e079
sydney13 <59878754+sydney13@users.noreply.github.com>
2021-04-19 13:44:04 -0600

    made all the forms look the same

--- edit_task.php
+++ edit_task.php
@@ -16,51 +16,53 @@ $arrlength = count($tasks);
 $familylength = count($family);
   
 for($x = 0; $x < $arrlength; $x++) {
-    echo ' <div style="margin: 150px; margin-top: 25px;">
-    <h1 class="title" >Edit Task</h1> 
-    <form action="update_task.php?task_id='. $tasks[$x]->getTaskId() .'" method="POST">
-        <div class="field">
-            <label class="label">Task Name</label>
-            <div class="control">
-                <input type="text" class="form-control" name="task_name" value="' . $tasks[$x]->getTaskName() . '"  placeholder="Task Name"/>
-            </div>
-        </div>
-        <div class="field">
-        <label class="label">Select Family Member</label>
-            <div class="class="control">
-                <select name="family_member_id">
-                    <option value="0">Select dropdown</option>]';
-                    for($y = 0; $y < $familylength; $y++){
-                    echo '<option value="'. $family[$y]->getFamilyMemberId() .'">' . $family[$y]->getFamilyMemberName() . '</option>';
-                    }
+    echo '<div style="margin: 150px; margin-top: 25px;">
+    <h1 class="title" >Edit Task</h1>
+    <form action="insert_task.php" method="POST">
+                <div class="field" style="width: 300px;">
+                    <label class="label">Task Name</label>
+                    <div class="control">
+                    <input type="text" class="input" name="task_name" placeholder="Task Name"/>
+                    </div>
+                </div>
+                <div class="field">
+            <label class="label">Select Family Member</label>
+                <div class="select">
+                    <select name="family_member_id">
+                        <option value="0">Select dropdown</option>';
+                        for($y = 0; $y < $familylength; $y++){
+                            echo '<option value="'. $family[$y]->getFamilyMemberId() .'">' . $family[$y]->getFamilyMemberName() . '</option>';
+                            }
                 echo '</select>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <label class="label">Start Date</label>
-            <div class="control">
-                <input type="datetime-local" class="form-control" name="task_start"/>
-            </div>
-        </div>
-        <div class="field">
-            <label class="label">End Date</label>
-            <div class="control">
-                <input type="datetime-local" class="form-control" name="task_end"/>
-            </div>
-        </div>
-        <div class="field">
-            <label class="label">Task Description</label>
-            <div class="control">
-                <textarea width="50pxpx" class="textarea" name="task_description" placeholder="Task description">' . $tasks[$x]->getTaskDesc() . '</textarea>
-            </div>
-        </div>
-        <div class="field">
-            <div class="control">
-                 <input type="submit" class="button is-success" value="Update Task">
-            </div>
-        </div>
-    </form>
-    </div>';
+                <div class="field" style="width: 250px;">
+                    <label class="label">Start Date</label>
+                    <div class="control">
+                    <input type="datetime-local" class="input" name="task_start"/>
+                    </div>
+                </div>
+                <div class="field" style="width: 250px;">
+                    <label class="label">End Date</label>
+                    <div class="control">
+                    <input type="datetime-local" class="input" name="task_end"/>
+                    </div>
+                </div>
+                <div class="field" style="width: 600px;">
+                    <label class="label">Task Description</label>
+                    <div class="control">
+                    <textarea width="50pxpx" class="textarea" name="task_description" placeholder="Task description"></textarea>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <input type="submit" class="button is-success" value="Add Task">
+                    </div>
+                </div>
+            </form>
+            <br>
+            <br>
+    </div';
 }
     require_once('./footer.php');
 
